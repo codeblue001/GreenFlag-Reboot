d92e9b2f6fe7dcd548138ee764c83eb831f2ca89
codeblue001 <39382665+codeblue001@users.noreply.github.com>
2019-09-12 09:23:15 -0400

    added radio button

---
+++ app/src/main/java/code/blue/greenflag/DatePickerFragment.java
@@ -0,0 +1,26 @@
+package code.blue.greenflag;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.widget.DatePicker;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+
+public class DatePickerFragment extends DialogFragment {
+    //https://www.youtube.com/watch?v=33BFCdL0Di0
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        return new DatePickerDialog(getActivity(), (DatePickerDialog.OnDateSetListener) getActivity(), year, month, day);
+    }
+}
--- app/src/main/java/code/blue/greenflag/Profile.java
+++ app/src/main/java/code/blue/greenflag/Profile.java
@@ -2,8 +2,10 @@ package code.blue.greenflag;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.Manifest;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -12,20 +14,21 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import java.text.DateFormat;
 import java.util.Calendar;
 
-public class Profile extends AppCompatActivity {
+public class Profile extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
     TextView ageTextView, genderTextView, addressTextView;
     EditText nameEditText, usernameEditText, passwordEditText;
     RadioGroup radioGroup;
     RadioButton femaleChoice, maleChoice, unspecifiedChoice;
-    Calendar calendar;
     Button birthDateButton, saveButton;
     ImageView profileImage, cameraButton;
     static final int REQUEST_IMAGE_CAPTURE = 1;
@@ -42,10 +45,6 @@ public class Profile extends AppCompatActivity {
         usernameEditText = findViewById(R.id.usernameEditText);
         passwordEditText = findViewById(R.id.passwordlEditText);
         radioGroup = findViewById(R.id.radioGroup);
-        femaleChoice = findViewById(R.id.femaleRadioButton);
-        maleChoice = findViewById(R.id.maleRadioButton);
-        unspecifiedChoice = findViewById(R.id.unspecifiedRadioButton);
-//        calendar = findViewById(R.id.);
         birthDateButton = findViewById(R.id.selectBirthDateButton);
         saveButton = findViewById(R.id.saveButton);
         cameraButton = findViewById(R.id.changePhotoButton);
@@ -67,6 +66,25 @@ public class Profile extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        birthDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment datepicker = new DatePickerFragment();
+                datepicker.show(getSupportFragmentManager(), "date picker");
+            }
+        });
+
+        //Todo Radio button
+        //https://www.youtube.com/watch?v=-SdM1EUO1tQ
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+                RadioButton radioButton = findViewById(checkedId);
+                //checkedId means selected button id
+                genderTextView.setText(radioButton.getText());
+            }
+        });
     }
 
     @Override
@@ -77,5 +95,29 @@ public class Profile extends AppCompatActivity {
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             profileImage.setImageBitmap(imageBitmap);
         }
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+        Calendar today = Calendar.getInstance();
+        Calendar dob = Calendar.getInstance();
+
+        dob.set(Calendar.YEAR, year);
+        dob.set(Calendar.MONTH, month);
+        dob.set(Calendar.DAY_OF_MONTH, day);
+        String currentDateString = DateFormat.getDateInstance().format(dob.getTime());
+
+        //Todo Calculate Age
+        //https://stackoverflow.com/questions/38967422/calculate-age-from-birthdate
+        int age = today.get(Calendar.YEAR) - dob.get(Calendar.YEAR);
+
+        if (today.get(Calendar.DAY_OF_YEAR) < dob.get(Calendar.DAY_OF_YEAR)){
+            age--;
+        }
+
+        Integer ageInt = new Integer(age);
+        String ageS = ageInt.toString();
+
+        ageTextView.setText(ageS);
     }
 }
